2b2708c9f1ed8adeac9dd7eeae079808b2d4edce
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-15 15:04:47 +0530

    design user schema

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsDate, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  surname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  birthdate: Date;
+}
